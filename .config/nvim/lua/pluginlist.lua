1f679e391f4387bfbabf545b705889c96b267906
redha <ahred.mbrk@gmail.com>
2024-11-06 20:44:57 +0100

    Adding oil and gitsigns, seems to have other problems as well

--- .config/nvim/lua/pluginlist.lua
+++ .config/nvim/lua/pluginlist.lua
@@ -16,6 +16,34 @@ return {
       })
     end,
   },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        -- Your desired settings here
+        signs = {
+          add = { text = "│" },
+          change = { text = "│" },
+          delete = { text = "_" },
+          -- ...
+        },
+        signcolumn = true,
+        numhl = false,
+        linehl = false,
+        word_diff = false,
+        watch_gitdir = { interval = 10 },
+      })
+    end,
+  },
   "neovim/nvim-lspconfig",
   "EdenEast/nightfox.nvim",
 }
