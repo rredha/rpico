1f0e37f8ff0b1da346b6c8257e29a0e5705f79c2
redha <ahred.mbrk@gmail.com>
2024-12-03 10:49:24 +0100

    Migrating to flakes

---
+++ nix/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Embedded system devShell : Raspberry Pi Pico (RP2040)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    basic-nixvim-flake.url = "github:rredha/neovim-basic/nix?dir=nixvim";
+    pico-sdk-nixpkgs.url = "github:nixpkgs/932fc16b263f26803d3960e4400bc13dde84a972";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              inputs.pico-sdk-nixpkgs.legacyPackages.${system}.pico-sdk
+              gcc-arm-embedded
+              gcc
+              cmake
+              luarocks
+              python3
+              fd
+              ripgrep
+              fzf
+            ];
+          };
+      }
+
+  };
+}
